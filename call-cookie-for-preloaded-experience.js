81115a97cd08b5455468a67faa3c4e20d94679f3
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2024-02-21 13:48:50 +0100

    Update call-cookie-for-preloaded-experience.js
    added check for existing cookie.

--- call-cookie-for-preloaded-experience.js
+++ call-cookie-for-preloaded-experience.js
@@ -1,4 +1,4 @@
-var cookieName = '[name]';
+var cookieName = 'io_ab_test_variant';
 
 function createCookie(name, value, days) {
     if (days) {
@@ -11,4 +11,24 @@ function createCookie(name, value, days) {
     document.cookie = name + "=" + value + expires + "; path=/";
 }
 
-createCookie(cookieName, '[variant]', 60);
+//add function that checks the cookie if it exists
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+if (getCookie(cookieName) === null) {
+	//console.log("cookie: creating");
+	createCookie(cookieName, 'A', 60);
+} else{
+	//console.log("cookie: skipping cookie exists");
+}
