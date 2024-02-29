8a1523d0a525411826b181b111e8074b376355d7
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2024-02-29 13:10:35 +0100

    Update load_jquery_library.js

--- load_jquery_library.js
+++ load_jquery_library.js
@@ -2,7 +2,9 @@ function loadScript(url, callback) {
   const script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = url;
-  script.onload = callback;
+  script.onload = function() {
+    callback($);
+  };
   document.head.appendChild(script);
 }
 
