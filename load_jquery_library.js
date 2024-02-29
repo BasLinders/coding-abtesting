44b8a0bd0b787bcdf7ee2a5213f6f438b2f3d4a5
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2024-02-29 12:11:05 +0100

    Create load_jquery_library.js

---
+++ load_jquery_library.js
@@ -0,0 +1,11 @@
+function loadScript(url, callback) {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+
+loadScript('https://code.jquery.com/jquery-3.6.0.min.js', function() {
+  // jQuery is now loaded and can be used in the code that follows in this function.
+});
