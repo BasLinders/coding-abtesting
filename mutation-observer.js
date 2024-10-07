4b4fa2b89ab5edcac7a1e68fb3a9cc3d29367760
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2024-10-07 15:41:53 +0200

    Update mutation-observer.js

--- mutation-observer.js
+++ mutation-observer.js
@@ -1,4 +1,4 @@
-//Mutation Observer	
+//Mutation Observer	jQuery
 const observer = new MutationObserver(function(mutationsList) {
     for (let mutation of mutationsList) {
         if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
@@ -9,6 +9,21 @@ const observer = new MutationObserver(function(mutationsList) {
                 replaceErrorMsg();
                 break;
             }
+        }
+    }
+});
+
+// Mutation Observer vanilla Javascript
+const observer = new MutationObserver(function (mutationsList) {
+    for (let mutation of mutationsList) {
+        if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+            mutation.addedNodes.forEach(node => {
+                // Only process element nodes (skip text/comment nodes)
+                if (node.nodeType === 1 && node.classList.contains('step1') && node.classList.contains('active')) {
+                    domReady(moveProductCards); // execute when document is ready
+                    observer.disconnect(); // disconnect after execution has been completed
+                }
+            });
         }
     }
 });
